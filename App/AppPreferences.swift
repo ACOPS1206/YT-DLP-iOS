b7d1017d5dc9648e731fb6ebe30862398d529aef
에이콥스 <aecopse12@gmail.com>
2026-09-19 17:24:40 +0900

    revert: remove accent color customization

--- App/AppPreferences.swift
+++ App/AppPreferences.swift
@@ -24,36 +24,6 @@ enum AppLanguage: String, CaseIterable, Identifiable {
     }
 }
 
-enum AccentColorChoice: String, CaseIterable, Identifiable {
-    case monochrome, blue, purple, pink, orange, green, yellow
-
-    var id: String { rawValue }
-
-    var title: String {
-        switch self {
-        case .monochrome: "모노크롬"
-        case .blue: "블루"
-        case .purple: "퍼플"
-        case .pink: "핑크"
-        case .orange: "오렌지"
-        case .green: "그린"
-        case .yellow: "옐로"
-        }
-    }
-
-    var color: Color {
-        switch self {
-        case .monochrome: .primary
-        case .blue: .blue
-        case .purple: .purple
-        case .pink: .pink
-        case .orange: .orange
-        case .green: .green
-        case .yellow: .yellow
-        }
-    }
-}
-
 func appText(_ korean: String, _ english: String) -> String {
     let raw = UserDefaults.standard.string(forKey: "appLanguage") ?? AppLanguage.system.rawValue
     return (AppLanguage(rawValue: raw) ?? .system).text(korean, english)
